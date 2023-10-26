c4aca13ca91cae55bc4eecefaaad0b7b5ad062e6
xframee <54414538+xframee@users.noreply.github.com>
2023-10-27 00:50:05 +0200

    added movieinfo model

--- Portfolio2/WebServer/Models/MovieInfoModel.cs
+++ Portfolio2/WebServer/Models/MovieInfoModel.cs
@@ -2,5 +2,15 @@ namespace WebServer.Models;
 
 public class MovieInfoModel
 {
-    
+    public string Url { get; set; } = string.Empty;
+    public string Id { get; set; }
+    public string Type { get; set; } = string.Empty;
+    public string PrimaryTitle { get; set; } = string.Empty;
+    public string OriginalTitle { get; set;} = string.Empty;
+    public string IsAdult { get; set; } = string.Empty;
+    public string StartYear { get; set; } = string.Empty;
+    public string EndYear { get; set; } = string.Empty;
+    public int RunTime { get; set; }
+    public string Poster { get; set; } = string.Empty;
+    public string Plot { get; set; } = string.Empty;
 }
