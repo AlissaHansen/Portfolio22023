b7c4e94113d9b435bb71caab2c428084a11f1e59
xframee <alexander@kmax.dk>
2023-10-30 09:07:07 +0100

    tested the joining works

--- Portfolio2/MyApp/Program.cs
+++ Portfolio2/MyApp/Program.cs
@@ -1,7 +1,14 @@
 ﻿using DataLayer;
+using Microsoft.EntityFrameworkCore;
 
-var ds = new DataService();
+//var ds = new DataService();
 
-var movieInfos = ds.GetMovieInfos(1, 10);
+var db = new MoviedbContext();
+
+foreach (var x in db.MovieInfos.Include(x => x.Genres)
+             .Take(10))
+{
+    Console.WriteLine($"{x.Id}, {string.Join(",", x.Genres.Select(x => x.GenreName))}");
+}
 
 
