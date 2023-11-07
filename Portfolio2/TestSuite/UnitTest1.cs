9d10ff0d9c25fefa1da3007e7daf1eef0f98c556
xframee <alexander@kmax.dk>
2023-11-07 17:15:12 +0100

    added datalayer tests for movieinfo

--- Portfolio2/TestSuite/UnitTest1.cs
+++ Portfolio2/TestSuite/UnitTest1.cs
@@ -1,4 +1,5 @@
 using DataLayer;
+using Xunit;
 
 namespace TestSuite;
 
@@ -23,6 +24,74 @@ public class UnitTest1
         var service = new DataService();
         var movies = service.GetMovieInfos(0, 20);
         Assert.Equal(55277,movies.count);
-        
+        Assert.NotNull(movies.movieInfos.First().PrimaryTitle);
     }
+
+    [Fact]
+    public void GetMovie_WithInvalidId_ReturnsNoMovie()
+    {
+        var service = new DataService();
+        var movie = service.GetMovieInfo("notARealTconst");
+        Assert.Null(movie);
+    }
+
+    [Fact]
+    public void GetMovie_WithValidId_ReturnsMovie()
+    {
+        var service = new DataService();
+        var movie = service.GetMovieInfo("tt0137204");
+        Assert.Equal("Joe Finds Grace", movie.PrimaryTitle);
+    }
+    
+    /* Users tests */
+
+    [Fact]
+    public void GetUser_WithValidUserId_RetunsUser()
+    {
+        var service = new DataService();
+        var user = service.GetUser("Alissa");
+        Assert.NotNull(user);
+    }
+
+    [Fact]
+    public void CreateUser_WithValidData_MakesUser()
+    {
+        var service = new DataService();
+        var user = new User
+        {
+            UserId = "Test",
+            Password = "mypswd",
+            Salt = "peber",
+            Role = "User"
+        };
+        var result = (service.CreateUser(user));
+        Assert.True(result);
+
+        service.DeleteUser("Test"); //fjern den nye user igen
+    }
+
+    [Fact]
+    public void DeleteUser_WithValidUserId_DeletesUser()
+    {
+        var service = new DataService();
+        var user = new User
+        {
+            UserId = "Test",
+            Password = "mypswd",
+            Salt = "peber",
+            Role = "User"
+        };
+        service.CreateUser(user);
+        var result = service.DeleteUser("Test");
+        Assert.True(result);
+    }
+
+    [Fact]
+    public void DeleteUser_WithInvalidId_ReturnsFalse()
+    {
+        var service = new DataService();
+        var result = service.DeleteUser("");
+        Assert.False(result);
+    }
+    
 }
