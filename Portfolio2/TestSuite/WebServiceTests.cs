88df9a3494dffd253497e215db58747dcab8db19
xframee <54414538+xframee@users.noreply.github.com>
2023-11-08 11:19:00 +0100

    added webservicetest

---
+++ Portfolio2/TestSuite/WebServiceTests.cs
@@ -0,0 +1,37 @@
+﻿using System.Net;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+
+namespace TestSuite;
+
+public class WebServiceTests
+{
+    private const string MoviesApi = "http://localhost:5001/api/movieinfos";
+
+    [Fact]
+    public async Task ApiMovies_GetMoviesNoArgument_OkAndAllMovies()
+    {
+        var (data, statusCode) = await GetObject(MoviesApi);
+        Assert.Equal(HttpStatusCode.OK, statusCode);
+    }
+
+
+    // Helpers
+
+    async Task<(JsonArray?, HttpStatusCode)> GetArray(string url)
+    {
+        var client = new HttpClient();
+        var response = client.GetAsync(url).Result;
+        var data = await response.Content.ReadAsStringAsync();
+        return (JsonSerializer.Deserialize<JsonArray>(data), response.StatusCode);
+    }
+
+    async Task<(JsonObject?, HttpStatusCode)> GetObject(string url)
+    {
+        var client = new HttpClient();
+        var response = client.GetAsync(url).Result;
+        var data = await response.Content.ReadAsStringAsync();
+        return (JsonSerializer.Deserialize<JsonObject>(data), response.StatusCode);
+    }
+
+}
