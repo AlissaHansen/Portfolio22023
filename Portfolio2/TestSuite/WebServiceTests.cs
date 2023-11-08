dd1fe42bdec18457a5bb1162bd0c208f4677c50d
xframee <54414538+xframee@users.noreply.github.com>
2023-11-08 13:01:02 +0100

    added helper class

--- Portfolio2/TestSuite/WebServiceTests.cs
+++ Portfolio2/TestSuite/WebServiceTests.cs
@@ -15,16 +15,23 @@ public class WebServiceTests
         Assert.Equal(HttpStatusCode.OK, statusCode);
     }
 
-
-    // Helpers
-
-    async Task<(JsonArray?, HttpStatusCode)> GetArray(string url)
+    [Fact]
+    public async Task ApiMovies_GetMovieWithValidTconst_OkAndMovie()
     {
-        var client = new HttpClient();
-        var response = client.GetAsync(url).Result;
-        var data = await response.Content.ReadAsStringAsync();
-        return (JsonSerializer.Deserialize<JsonArray>(data), response.StatusCode);
+        var (movie, statusCode) = await GetObject($"{MoviesApi}/tt0499549");
+        Assert.Equal(HttpStatusCode.OK, statusCode);
+        Assert.Equal("Avatar", movie?.Value("primaryTitle"));
     }
+
+    [Fact]
+    public async Task ApiMovie_GetMovieWithInvalidTconst_NotFound()
+    {
+        var (_, statusCode) = await GetObject($"{MoviesApi}/notATconst");
+        Assert.Equal(HttpStatusCode.NotFound, statusCode);
+    }
+
+
+    // Helpers taget fra Henriks test
 
     async Task<(JsonObject?, HttpStatusCode)> GetObject(string url)
     {
@@ -33,5 +40,13 @@ public class WebServiceTests
         var data = await response.Content.ReadAsStringAsync();
         return (JsonSerializer.Deserialize<JsonObject>(data), response.StatusCode);
     }
+}
 
+static class HelperExt
+{
+    public static string? Value(this JsonNode node, string name)
+    {
+        var value = node[name];
+        return value?.ToString();
+    }
 }
