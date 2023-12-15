40c1c74dfd80ed6abfb9235788d1ae2d49a18730
Alissa Hansen <79077747+AlissaHansen@users.noreply.github.com>
2023-12-15 15:40:06 +0100

    Update MovieSearchModel.cs

--- Portfolio2/WebServer/Models/MovieSearchModel.cs
+++ Portfolio2/WebServer/Models/MovieSearchModel.cs
@@ -3,6 +3,7 @@ namespace WebServer.Models;
 public class MovieSearchModel
 {
     public string Url { get; set; }
+    public string MovieInfoId { get; set; }
     public string Title { get; set; }
     public string Poster { get; set; } = String.Empty;
 }
